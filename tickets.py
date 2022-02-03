86f8efbb59889891432fc32ebe587b58278bfce1
George <pirat_yt@mail.ru>
2022-02-03 22:34:49 +0300

    A new file added

---
+++ tickets.py
@@ -0,0 +1,15 @@
+tickets = int(input("введите количство билетов"))
+age = int(input("Введите возраст"))
+cost = 0
+print(type(age))
+for(i in range(tickets)
+  if age >= 18 & age <= 25:
+    cost + 990 * tickets
+    elif age > 25:
+      cost + 1390 * tickets
+    else:
+      cost = 0
+    if tickets >= 3:
+      cost = - cost / 100 * 10
+ 
+print("total cost = ", cost)
